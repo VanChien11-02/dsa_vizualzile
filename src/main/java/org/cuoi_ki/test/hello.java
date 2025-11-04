bd4b1374f473764190aba6ec8de16c889bcebd98
vchiendev <anhminhoppoa15@gmail.com>
2025-11-04 21:36:03 +0700

    test 2.0

--- src/main/java/org/cuoi_ki/test/hello.java
+++ src/main/java/org/cuoi_ki/test/hello.java
@@ -3,5 +3,6 @@ package org.cuoi_ki.test;
 public class hello {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("good bye");
     }
 }
